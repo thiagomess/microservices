d941524e68d05de6b65c9d63a090b595d3e3ef71
Thiago <thiagogomes19@hotmail.com>
2021-04-11 19:28:33 -0300

    hr-payment implementation rest

---
+++ hr-payroll/src/main/java/com/gomes/hrpayroll/entity/Worker.java
@@ -0,0 +1,18 @@
+package com.gomes.hrpayroll.entity;
+
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+}
